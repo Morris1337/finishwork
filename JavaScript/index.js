09701356458a03a2ae7fa6770a365d0462c35729
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-09-26 14:19:42 +0300

    Java Script 3 update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,51 +1,72 @@
 const slideBtns = document.querySelectorAll(".slide_conteiner")
 const slideNames = document.querySelectorAll(".section_2_li_style")
-const slidePhoto = document.querySelectorAll(".eye")
+const slidePhoto = document.querySelectorAll(".photo_hero2")
 
 const prev = document.getElementById("leftArrow")
 const next = document.getElementById("rightArrow")
 
-let currentIndex = 0
+let currentIndex = 0;
 
 function changeImage (imageIndex){
     slidePhoto.forEach((photo) => {
         photo.style.display = "none";
         })
     slidePhoto[imageIndex].style.display = "block"
+    changeTextName()
 }
 
 changeImage(0)
 
+
+function changeTextName(){
+    slideNames.forEach((name, index) =>{
+        if(index === currentIndex){
+            name.classList.add("clicked")
+        }else{
+            name.classList.remove("clicked")
+        }
+    })
+
+    slideBtns.forEach((name, index) => {
+        if(index === currentIndex){
+            name.classList.add("push")
+        }else{
+            name.classList.remove("push")
+        }
+    })
+}
+
 function btnChange(){
     slideNames.forEach((name, index) =>{
         name.addEventListener("click", ()=>{
-            changeImage(index);
-            if(name.classList.contains("clicked")){
-                name.style.color = "#E3B873"
-                name.style.textDecoration = "underline;"
-                name.classList.add("clicked");
-            }else{
-                name.style.color = "";
-                name.style.textDecoration = "";
-                name.classList.remove("clicked");
-            }
+            currentIndex = index
+            changeImage(index)
         })
     })
     
     slideBtns.forEach((name, index) =>{
         name.addEventListener("click", ()=>{
+            currentIndex = index
             changeImage(index)
         })
     })
 
     prev.addEventListener("click", ()=>{
-        changeImage(slidePhoto - 1);
-        currentIndex -= 1;
+        if(currentIndex === 0){
+            currentIndex = slidePhoto.length - 1;
+        }else{
+            currentIndex -=1;
+        }
+        changeImage(currentIndex); 
     })
     
-    prev.addEventListener("click", ()=>{
-        changeImage(currentIndex + 1);
-        currentIndex += 1;
+    next.addEventListener("click", ()=>{
+        if(currentIndex === slidePhoto.length - 1){
+            currentIndex = 0;
+        }else{
+            currentIndex +=1;
+        }
+        changeImage(currentIndex); 
     })
 }
 
